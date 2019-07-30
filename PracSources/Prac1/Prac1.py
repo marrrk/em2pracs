7f2cd9259dd961a81751f5473e0941c1409e5a37
Mark Njoroge <marksnjoroge@gmail.com>
2019-07-30 16:58:02 +0200

    3 bit counter with comments

--- PracSources/Prac1/Prac1.py
+++ PracSources/Prac1/Prac1.py
@@ -14,23 +14,23 @@ import RPi.GPIO as GPIO
 import time
 import itertools
 
-values = list(itertools.product(range(2), repeat=3))
-output_list = [11,12,13]
-input_list = [35,36]
-current = 0
+values = list(itertools.product(range(2), repeat=3))    #creates list of outputs
+output_list = [11,12,13]     #list of output pins
+input_list = [35,36]	      #list of input pins
+current = 0       #initialises current value in count
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(output_list,GPIO.OUT)
-GPIO.setup(input_list,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(output_list,GPIO.OUT)		#sets up output pins
+GPIO.setup(input_list,GPIO.IN,pull_up_down=GPIO.PUD_UP)			#sets up input pins
 
-def LEDon():
+def LEDon():			#simple function that turns LED on and off
 	GPIO.output(11, GPIO.HIGH)
 	time.sleep(1)
 	GPIO.output(11, GPIO.LOW)
 	time.sleep(1)
 
 
-def callback_1(self):
+def callback_1(self):  			#callback function that subtracts from the value
 	print("subtracted")
 	global current
 	if current == 0:
@@ -40,7 +40,7 @@ def callback_1(self):
 	GPIO.output(output_list,values[current])
 
 
-def callback_2(self):
+def callback_2(self):			#callback function that adds to the value
 	print("added")
 	global current
 	if current == 7:
@@ -49,7 +49,7 @@ def callback_2(self):
 		current +=1
 	GPIO.output(output_list,values[current])
 
-GPIO.add_event_detect(35,GPIO.FALLING,callback=callback_1,bouncetime=300)
+GPIO.add_event_detect(35,GPIO.FALLING,callback=callback_1,bouncetime=300)	#detects when a button is pressed
 GPIO.add_event_detect(36,GPIO.FALLING,callback=callback_2,bouncetime=300)
 
 
